72de0ecd02dba1e5c208c8543e425da2327cd83e
Guoaking <775846611@qq.com>
2020-06-19 10:54:50 +0800

    6-19更新

--- JS/Module.js
+++ JS/Module.js
@@ -1,1 +1,51 @@
-Array()
+
+//将时间戳转换成正常时间格式
+
+
+
+
+
+
+let d = "2020-06-12T16:00:00.000Z"
+let e = new Date(d)
+
+
+Date.prototype.Format = function (fmt) {
+    var o = {
+            "M+": this.getMonth() + 1, // 月份
+            "d+": this.getDate(), // 日
+            "h+": this.getHours(), // 小时
+            "m+": this.getMinutes(), // 分
+            "s+": this.getSeconds(), // 秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+            "S": this.getMilliseconds() // 毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + ""));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+ function Formata (date,fmt) {
+    var o = {
+            "M+": date.getMonth() + 1, // 月份
+            "d+": date.getDate(), // 日
+            "h+": date.getHours(), // 小时
+            "m+": date.getMinutes(), // 分
+            "s+": date.getSeconds(), // 秒
+            "q+": Math.floor((date.getMonth() + 3) / 3), // 季度
+            "S": date.getMilliseconds() // 毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + ""));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+//console.log(new Date(e).Format('yy-MM-dd'))
+//console.log(e)
+
+console.log(Formata(e,'yy-MM-dd'))
+console.log(e,"22")
+
