dd616595afee583447be0ccae586a9bb69a5e160
Guoaking <775846611@qq.com>
2020-09-16 20:28:02 +0800

    9-16 提交

--- JS/测试用.js
+++ JS/测试用.js
@@ -1,71 +1,165 @@
-[{
-  "errcode": 0,
-  "page":0,
-  "page_count":10,
-  "data": [
-    {
-      "title": "情报标题",
-      "like_label_id":"兴趣标签id",
-      "like_label":"兴趣标签",
-      "break_law_label":"违法标签",
-      "break_law_label_id":"违法标签id",
-      "factory_source_name": "关联共治平台",
-      "factory_source_id":"共治平台id",
-      "source_txt_two": "详情页文本",
-      "source_pic_two": "详情页图片",
-      "source_mv_two": "详情页视频",
-      "source_picshot_two": "详情截图",
-      "app_name": "应用名称",
-      "media_name": "媒体名称",
-      "app_package": "应用包名",
-      "illegal_name": "违法主体",
-      "page_add_one": "源url",
-      "source_txt_one": "源文本",
-      "source_pic_one": "源图片",
-      "source_mv_one": "源视频",
-      "source_picshot_two": "详情截图",
-      "source_url_two": "二级url",
-      "date_collect_time": "广告时间(违法时间)",
-      "break_law_enterprise": "违法主体",
-      "break_law_type": "违法类型",
-      "push_user": "举报人",
-      "push_u_phone": "联系电话",
-      "use_law":"使用法律条目",
-     "province_name":"省",
-      "province_id":"省id",
-      "city_name":"市",
-      "city_id":"市id",
-      "county_name":"区",
-      "county_id":"区id",
-      "town_name":"镇",
-      "town_id":"镇id",
-      
-      //违法信息
-      "break_law_info": {
-        "b_type":"商业类别",
-        "entry":"主体",
-        "ad_master":"广告主",
-        "brand":"品牌",
-        "version_info":"版本说明"
-      },
-      //可能有多个违法判定
-      "break_law_res": [
-        {
-          "break_law_level": "违法程度",
-          "break_law_res": "违法表现",
-          "break_law_content": "违法内容",
-          "product_num": "生产批准文号",
-          "ad_product_num": "广告中生产批准文号",
-          "ad_shot": "网页截图"
-        }, {
-          "break_law_level": "违法程度",
-          "break_law_performance": "违法表现",
-          "break_law_content": "违法内容",
-          "product_num": "生产批准文号",
-          "ad_product_num": "广告中生产批准文号",
-          "ad_shot": "网页截图"
+var bea= [
+
+  {
+    "self_id": "CT0001002",
+    "parent_id": "CT0001",
+    "name": "文明办",
+    "level": 1,
+    "tag_name": "文明办",
+    "_id": "CT0001002"
+  },
+  
+  {
+    "self_id": "CT0001001",
+    "parent_id": "CT0001",
+    "name": "市场监督管理局",
+    "level": 1,
+    "tag_name": "市场监督管理局",
+    "_id": "CT0001001"
+  },
+  {
+    "self_id": "CT0002006",
+    "parent_id": "CT0002",
+    "name": "农药",
+    "level": 1,
+    "tag_name": "农药",
+    "_id": "CT0002006"
+  },
+  {
+    "self_id": "CT0002001",
+    "parent_id": "CT0002",
+    "name": "药品",
+    "level": 1,
+    "tag_name": "药品",
+    "_id": "CT0002001"
+  },
+  {
+    "self_id": "CT0003001",
+    "parent_id": "CT0003",
+    "name": "市容环境管理",
+    "level": 1,
+    "tag_name": "市容环境管理",
+    "_id": "CT0003001"
+  },
+  {
+    "self_id": "CT0002011",
+    "parent_id": "CT0002",
+    "name": "房地产",
+    "level": 1,
+    "tag_name": "房地产",
+    "_id": "CT0002011"
+  },
+  {
+    "self_id": "CT0002010",
+    "parent_id": "CT0002",
+    "name": "化妆品",
+    "level": 1,
+    "tag_name": "化妆品",
+    "_id": "CT0002010"
+  },
+  {
+    "self_id": "CT0003007",
+    "parent_id": "CT0003",
+    "name": "交通管理类",
+    "tag_name": "交通管理类",
+    "_id": "CT0003007"
+  },
+  {
+    "self_id": "CT0003005",
+    "parent_id": "CT0003",
+    "name": "教育管理类",
+    "tag_name": "教育管理类",
+    "_id": "CT0003005"
+  },
+  {
+    "self_id": "CT0003003",
+    "parent_id": "CT0003",
+    "name": "市场管理类",
+    "tag_name": "市场管理类",
+    "_id": "CT0003003"
+  },
+  {
+    "self_id": "CT0002009",
+    "parent_id": "CT0002",
+    "name": "酒类",
+    "level": 1,
+    "tag_name": "酒类",
+    "_id": "CT0002009"
+  },
+  {
+    "self_id": "CT0002008",
+    "parent_id": "CT0002",
+    "name": "烟草",
+    "level": 1,
+    "tag_name": "烟草",
+    "_id": "CT0002008"
+  },
+  {
+    "self_id": "CT0003010",
+    "parent_id": "CT0003",
+    "name": "园林绿化类",
+    "tag_name": "园林绿化类",
+    "_id": "CT0003010"
+  },
+  {
+    "self_id": "CT0003006",
+    "parent_id": "CT0003",
+    "name": "社会服务类",
+    "tag_name": "社会服务类",
+    "_id": "CT0003006"
+  },
+  {
+    "self_id": "CT0003002",
+    "parent_id": "CT0003",
+    "name": "建设管理类",
+    "tag_name": "建设管理类",
+    "_id": "CT0003002"
+  }
+
+]
+
+var afa = [
+
+
+
+  {
+    "self_id": "CT0001002",
+    "parent_id": "CT0001",
+    "name": "文明办",
+    "level": 1,
+    "tag_name": "文明办",
+    "_id": "CT0001002"
+  },
+  {
+    "self_id": "CT0001001",
+    "parent_id": "CT0001",
+    "name": "市场监督管理局",
+    "level": 1,
+    "tag_name": "市场监督管理局",
+    "_id": "CT0001001"
+  },  {
+    "self_id": "CT000309902",
+    "parent_id": "CT0003",
+    "name": "建设管理类",
+    "tag_name": "建设管理类",
+    "_id": "CT0003002"
+  }
+
+]
+
+function a (data1,data2){
+  
+    for(var i = 0;i < data1.length; i++){
+      for(var j = 0;j < data2.length; j++){
+        if(data1.length>0){
+         if(data1[i].self_id == data2[j].self_id){
+            data1.splice(i,1);
         }
-      ]
+      }
     }
-  ]
-}]
+  }
+
+  return data1;
+}
+
+console.log(a(afa,bea))
--- leet_code/53.最大子序和.js
+++ leet_code/53.最大子序和.js
@@ -9,8 +9,59 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
 
 };
 // @lc code=end
 
+function() {
+    let arr = #data#
+    let arrz = #dat#
+
+
+    if (arr.length > 0) {
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i].break_law_level) {
+                case "CD002":
+                    arr[i].break_law_level = "违法程度-中"
+                    break;
+                case "CD001":
+                    arr[i].break_law_level = "违法程度-低"
+                    break;
+                case "CD003":
+                    arr[i].break_law_level = "违法程度-高"
+                    break;
+            }
+
+            var a = arr[i].date_collect_time;
+            var b = a.substr(0, a.indexOf(" "))
+            arr[i].date_collect_time = b
+
+            /*================================*/
+            if (arrz.length > 0) {
+                for (let j = 0; j < arrz.length; j++) {
+                    if (arrz[j].uuid == arr[i].uuid) {
+                        arr[i].views_num = arrz[j].views_num
+                        arr[i].tagging_num = arrz[j].tagging_num
+                        break;
+                    } else {
+                        arr[i].views_num = 0
+                        arr[i].tagging_num = 0
+                    }
+                }
+            } else {
+                arr[i].views_num = 0
+                arr[i].tagging_num = 0
+
+            }
+
+ var a 
+ if(arr[i].break_law_level!=null){ a =a+"|" +arr[i].break_law_level}
+ if(arr[i].break_law_label!=null) {a =a+"|"+ arr[i].break_law_label}
+ arr[i]['label'] = a
+        }
+
+
+    }
+
+}
